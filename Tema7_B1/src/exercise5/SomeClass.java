631759ce667eb5ff34c3e4dabacbb387641057b6
ikloerzer <ikloerzer@106-23.info2>
2023-03-23 13:49:21 +0100

    Terminar el último exercise

---
+++ Tema7_B1/src/exercise5/SomeClass.java
@@ -0,0 +1,28 @@
+package exercise5;
+
+public class SomeClass {
+	
+	private int x;
+
+	public SomeClass() {
+		x=10;
+	}
+	
+	public SomeClass(int xIn) {
+		
+		x=xIn;
+	}
+	
+	public void setX(int xIn) {
+		
+		x=xIn;
+	}
+	
+	public int getX() {
+		
+		return x;
+	}
+	
+	
+
+}
---
+++ Tema7_B1/src/exercise5/Test1.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int y=20;
+		SomeClass myObject= new SomeClass(5);
+		System.out.println(myObject.getX());
+		test (y, myObject);
+	}
+
+}
