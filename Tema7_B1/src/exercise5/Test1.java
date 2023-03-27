2b939827114db8cc1bf6aa0d4761d93159e5750a
ikloerzer <ikloerzer@106-23.info2>
2023-03-27 10:15:14 +0200

    He terminado los exercises

--- Tema7_B1/src/exercise5/Test1.java
+++ Tema7_B1/src/exercise5/Test1.java
@@ -7,6 +7,14 @@ public class Test1 {
 		SomeClass myObject= new SomeClass(5);
 		System.out.println(myObject.getX());
 		test (y, myObject);
+		System.out.println(y);
+		System.out.println(myObject.getX());
+		
+	}
+	
+	static void test (int z, SomeClass classIn) {
+		z=50;
+		classIn.setX(100);
 	}
 
 }
